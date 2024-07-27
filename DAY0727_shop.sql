e022c1585a8fc9f53a96452bee56d12e38086018
Kangmin kim <127845112+adorahelen@users.noreply.github.com>
2024-07-27 15:57:41 +0900

    add data to shopDB

---
+++ DAY0727_shop.sql
@@ -0,0 +1,15 @@
+-- show databases;
+-- use shopdb;
+-- show tables;
+
+-- INSERT INTO t_product VALUES('컴퓨터', 10, '2021-01-01', '삼성', 17);
+-- INSERT INTO t_product VALUES('세탁기', 20, '2022-09-01', '엘지', 3);
+-- INSERT INTO t_product VALUES('냉장고', 5, '2023-02-01', '대우', 22);
+
+-- INSERT INTO t_member VALUES('dong', '홍길동', '강원도 동해시');
+-- INSERT INTO t_member VALUES('seo', '홍길서', '서울 서대문구');
+-- INSERT INTO t_member VALUES('nam', '홍길남', '충남 태안시');
+-- INSERT INTO t_member VALUES('buk', '홍길북', '경상북도 울릉군');
+
+-- select * from t_member;
+-- select *from t_product;
