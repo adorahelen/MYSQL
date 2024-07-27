e5c6342b8afe12fcd9162a315f447fcbb9ab11f6
Kangmin kim <127845112+adorahelen@users.noreply.github.com>
2024-07-27 15:03:04 +0900

    admin_dataExport_emp

---
+++ BackUpDB.sql
@@ -0,0 +1,189 @@
+CREATE DATABASE  IF NOT EXISTS `employees` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `employees`;
+-- MySQL dump 10.13  Distrib 8.0.34, for macos13 (arm64)
+--
+-- Host: localhost    Database: employees
+-- ------------------------------------------------------
+-- Server version	8.0.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `BONUS`
+--
+
+DROP TABLE IF EXISTS `BONUS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `BONUS` (
+  `ENAME` varchar(10) DEFAULT NULL,
+  `JOB` varchar(9) DEFAULT NULL,
+  `SAL` int DEFAULT NULL,
+  `COMM` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BONUS`
+--
+
+LOCK TABLES `BONUS` WRITE;
+/*!40000 ALTER TABLE `BONUS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `BONUS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `DEPT`
+--
+
+DROP TABLE IF EXISTS `DEPT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `DEPT` (
+  `DEPTNO` int NOT NULL,
+  `DNAME` varchar(14) DEFAULT NULL,
+  `LOC` varchar(13) DEFAULT NULL,
+  PRIMARY KEY (`DEPTNO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `DEPT`
+--
+
+LOCK TABLES `DEPT` WRITE;
+/*!40000 ALTER TABLE `DEPT` DISABLE KEYS */;
+INSERT INTO `DEPT` VALUES (10,'ACCOUNTING','NEW YORK'),(20,'RESEARCH','DALLAS'),(30,'SALES','CHICAGO'),(40,'OPERATIONS','BOSTON');
+/*!40000 ALTER TABLE `DEPT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `EMP`
+--
+
+DROP TABLE IF EXISTS `EMP`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `EMP` (
+  `EMPNO` int NOT NULL,
+  `ENAME` varchar(10) DEFAULT NULL,
+  `JOB` varchar(9) DEFAULT NULL,
+  `MGR` int DEFAULT NULL,
+  `HIREDATE` date DEFAULT NULL,
+  `SAL` int DEFAULT NULL,
+  `COMM` int DEFAULT NULL,
+  `DEPTNO` int DEFAULT NULL,
+  PRIMARY KEY (`EMPNO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `EMP`
+--
+
+LOCK TABLES `EMP` WRITE;
+/*!40000 ALTER TABLE `EMP` DISABLE KEYS */;
+INSERT INTO `EMP` VALUES (7369,'SMITH','CLERK',7902,'1980-12-17',800,NULL,20),(7499,'ALLEN','SALESMAN',7698,'1981-02-20',1600,300,30),(7521,'WARD','SALESMAN',7698,'1981-02-22',1250,500,30),(7566,'JONES','MANAGER',7839,'1981-04-02',2975,NULL,20),(7654,'MARTIN','SALESMAN',7698,'1981-09-28',1250,1400,30),(7698,'BLAKE','MANAGER',7839,'1981-05-01',2850,NULL,30),(7782,'CLARK','MANAGER',7839,'1981-06-09',2450,NULL,10),(7788,'SCOTT','ANALYST',7566,'1987-04-19',3000,NULL,20),(7839,'KING','PRESIDENT',NULL,'1981-11-17',5000,NULL,10),(7844,'TURNER','SALESMAN',7698,'1981-09-08',1500,0,30),(7876,'ADAMS','CLERK',7788,'1987-05-23',1100,NULL,20),(7900,'JAMES','CLERK',7698,'1981-12-03',950,NULL,30),(7902,'FORD','ANALYST',7566,'1981-12-03',3000,NULL,20),(7934,'MILLER','CLERK',7782,'1982-01-23',1300,NULL,10);
+/*!40000 ALTER TABLE `EMP` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `SALGRADE`
+--
+
+DROP TABLE IF EXISTS `SALGRADE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `SALGRADE` (
+  `GRADE` int DEFAULT NULL,
+  `LOSAL` int DEFAULT NULL,
+  `HISAL` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `SALGRADE`
+--
+
+LOCK TABLES `SALGRADE` WRITE;
+/*!40000 ALTER TABLE `SALGRADE` DISABLE KEYS */;
+INSERT INTO `SALGRADE` VALUES (1,700,1200),(2,1201,1400),(3,1401,2000),(4,2001,3000),(5,3001,9999);
+/*!40000 ALTER TABLE `SALGRADE` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_emp`
+--
+
+DROP TABLE IF EXISTS `t_emp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `t_emp` (
+  `empno` int NOT NULL AUTO_INCREMENT,
+  `enamel` varchar(10) DEFAULT NULL,
+  `deptno` int DEFAULT NULL,
+  `sal` int DEFAULT NULL,
+  `hiredate` date DEFAULT NULL,
+  PRIMARY KEY (`empno`)
+) ENGINE=InnoDB AUTO_INCREMENT=9003 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_emp`
+--
+
+LOCK TABLES `t_emp` WRITE;
+/*!40000 ALTER TABLE `t_emp` DISABLE KEYS */;
+INSERT INTO `t_emp` VALUES (1000,'kim',40,1000,'2020-10-10'),(2000,'kim',40,1000,'2020-10-10'),(3000,'sam',90,NULL,'2024-07-26'),(7782,'CLARK',10,2450,'1981-06-09'),(7839,'KING',10,5000,'1981-11-17'),(7934,'MILLER',10,1300,'1982-01-23'),(7935,'sara',90,NULL,'2024-07-26'),(9000,'tester',90,9000,'2024-07-26'),(9001,'uploader',90,9000,'2024-07-26'),(9002,'tester',90,9000,'2024-07-26');
+/*!40000 ALTER TABLE `t_emp` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_temp2`
+--
+
+DROP TABLE IF EXISTS `t_temp2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `t_temp2` (
+  `empno` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_temp2`
+--
+
+LOCK TABLES `t_temp2` WRITE;
+/*!40000 ALTER TABLE `t_temp2` DISABLE KEYS */;
+INSERT INTO `t_temp2` VALUES (7369),(7499),(7521),(7566),(7654),(7698),(7782),(7788),(7839),(7844),(7876),(7900),(7902),(7934);
+/*!40000 ALTER TABLE `t_temp2` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'employees'
+--
+
+--
+-- Dumping routines for database 'employees'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-26 17:49:29
